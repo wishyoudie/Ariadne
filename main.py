ee7926b9e10c6027cbb53af349e72410fc7aa208
wishyoudie <66038379+wishyoudie@users.noreply.github.com>
2021-12-20 04:58:45 +0300

    Commit #1 First steps in creating Ariadne. Figured out with API, created very basic start.

---
+++ main.py
@@ -0,0 +1,22 @@
+import telebot
+from helpers import *
+
+ariadne = telebot.TeleBot(config['token'])
+
+
+@ariadne.message_handler(commands=['start'])
+def command_start(message):
+    ariadne.send_message(message.chat.id, 'Hi! My name is Ariadne. I will be your virtual assistant ❤')
+    ariadne.send_message(message.chat.id, 'All of my available texts: ' + getDictionaryValues(supported_texts))
+
+
+@ariadne.message_handler(content_types=['text'])
+def get_text(message):
+    txt = message.text.lower()
+    if txt in supported_texts:
+        ariadne.send_message(message.chat.id, supported_texts[txt])
+    else:
+        ariadne.send_message(message.chat.id, 'Sorry, I don\'t support this text yet :(')
+
+
+ariadne.polling(none_stop=True, interval=0)
